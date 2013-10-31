17106ed8a6ff02384cb13ce98a530c28ea95be8b
Jesper Eskilson <jesper@eskilson.se>
2013-10-31 16:54:42 +0100

    Updated emacs.el to work better on Windows

---
+++ elisp/emacs-custom-win32.el
@@ -0,0 +1,25 @@
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 98 :width normal))))
+ '(header-line ((t (:inherit mode-line :background "gainsboro" :foreground "dark green" :box nil)))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(clang "clang")
+ '(column-number-mode t)
+ '(custom-enabled-themes (quote (adwaita)))
+ '(display-time-24hr-format t)
+ '(display-time-mode t)
+ '(flycheck-clang-include-path (quote ("/usr/lib/llvm-3.4/include")))
+ '(inhibit-startup-screen t)
+ '(read-file-name-completion-ignore-case t)
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
