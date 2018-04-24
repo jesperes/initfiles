4213182a87f346b735194c9d5853a4c2482c96a7
Jesper Eskilson <jesper.eskilson@iar.com>
2018-04-24 13:03:54 +0200

    Use fancy git prompt

--- bashrc.sh
+++ bashrc.sh
@@ -34,5 +34,11 @@ alias ls='ls -l --color=auto'
 alias sweep='find -type f -name \*~ -exec rm -vf {} \;'
 
 #
+# Git prompt
+# GIT_PROMPT_ONLY_IN_REPO=1
+source ~/dev/bash-git-prompt/gitprompt.sh 
+
+#
 # Interactive prompt.
-export PS1="\[$(tput bold)\]\[$(tput setaf 6)\]\t \[$(tput setaf 2)\][\[$(tput setaf 5)\]\u\[$(tput setaf 1)\]@\[$(tput setaf 4)\]\h \[$(tput setaf 6)\]\w\[$(tput setaf 2)\]]\[$(tput setaf 4)\]\\$ \[$(tput sgr0)\]"
+# export PS1="\[$(tput bold)\]\[$(tput setaf 6)\]\t \[$(tput setaf 2)\][\[$(tput setaf 5)\]\u\[$(tput setaf 1)\]@\[$(tput setaf 4)\]\h \[$(tput setaf 6)\]\w\[$(tput setaf 2)\]]\[$(tput setaf 4)\]\\$ \[$(tput sgr0)\]"
+
